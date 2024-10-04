77be5fe36cf9c571bd544a0f930c61947ac041b4
hscoelho <henriquescharlaucoelho@gmail.com>
2024-10-04 15:01:44 -0300

    fullscreen wezterm keybinding removed

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,6 +6,15 @@ config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 16
 config.color_scheme = "Gruvbox dark, hard (base16)"
 
+-- TODO: mudar dependendo do os
 config.default_prog = { "C:/Program Files/nu/bin/nu.exe" }
+
+config.keys = {
+	{
+		key = "Enter",
+		mods = "ALT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
 
 return config
