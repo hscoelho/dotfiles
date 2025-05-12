1bd11fd3ad3d1d70b82334e6b14ddd720a2e7be0
hscoelho <henriquescharlaucoelho@gmail.com>
2025-05-12 10:16:08 -0300

    stay-centered added to neovim
    + updates

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -1,5 +1,4 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 
-vim.o.scrolloff = 999
 vim.o.shell = "nu"
