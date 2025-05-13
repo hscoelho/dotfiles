1b08d6c6d773e76297d91a18e7af172af972aa11
hscoelho <henriquescharlaucoelho@gmail.com>
2025-05-13 08:53:59 -0300

    fixed stay-centered

--- lazyvim/lua/plugins/stay-centered.lua
+++ lazyvim/lua/plugins/stay-centered.lua
@@ -1,3 +1,18 @@
 return {
-  "arnamak/stay-centered.nvim",
+  {
+    "arnamak/stay-centered.nvim",
+    lazy = false,
+    opts = {
+      enabled = true,
+      allow_scroll_move = true,
+    },
+  },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      scroll = {
+        enabled = false,
+      },
+    },
+  },
 }
