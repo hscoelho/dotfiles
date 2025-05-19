64041bad2b4e9ee45628b643ef420d6c3d7870aa
hscoelho <henriquescharlaucoelho@gmail.com>
2025-05-19 15:10:05 -0300

    added sort by recency to bufferline

--- lazyvim/lua/plugins/bufferline.lua
+++ lazyvim/lua/plugins/bufferline.lua
@@ -7,6 +7,11 @@ return {
           return true
         end
       end,
+      sort_by = function(buffer_a, buffer_b)
+        local info_a = vim.fn.getbufinfo(buffer_a.id)[1]
+        local info_b = vim.fn.getbufinfo(buffer_b.id)[1]
+        return info_a.lastused > info_b.lastused
+      end,
     },
   },
 }
