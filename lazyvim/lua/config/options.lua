a295ee291d1b574d68298fb060fd28596670c37e
hscoelho <henriquescharlaucoelho@gmail.com>
2025-05-26 15:27:52 -0300

    added spellcheck to neovim

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -1,2 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+--
+vim.opt.spell = true
+vim.opt.spelllang = { "en_us", "pt_br" }
