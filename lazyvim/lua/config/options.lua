d076dc9f2e7b7945edc6b8c6e6375a6f1ca02da8
hscoelho <henriquescharlaucoelho@gmail.com>
2025-06-02 09:36:48 -0300

    added camelcase option to spell

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -1,6 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
---
 vim.opt.spell = true
 vim.opt.spelllang = { "en_us", "pt_br" }
 vim.opt.termguicolors = true
+vim.opt.spelloptions = "camel"
