371720adbfc6c20e5754a589978d42a76fb3076a
hscoelho <henriquescharlaucoelho@gmail.com>
2025-08-18 14:45:33 -0300

    changed copilot  to binary

--- lazyvim/lua/plugins/copilot.lua
+++ lazyvim/lua/plugins/copilot.lua
@@ -1,6 +1,7 @@
 return {
   "zbirenbaum/copilot.lua",
   opts = {
-    copilot_node_command = vim.fn.expand("$HOME") .. "/.local/share/fnm/node-versions/v22.18.0/installation/bin/node",
+    -- copilot_node_command = vim.fn.expand("$HOME") .. "/.local/share/fnm/node-versions/v22.18.0/installation/bin/node",
+    type = "binary",
   },
 }
