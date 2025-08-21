bbfaaa23b39ae274df23c142a62536661ccb6068
hscoelho <henriquescharlaucoelho@gmail.com>
2025-08-21 16:50:52 -0300

    added astronvim

---
+++ astronvim/lua/community.lua
@@ -0,0 +1,12 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.recipes.heirline-vscode-winbar" },
+  { import = "astrocommunity.file-explorer.mini-files" },
+}
