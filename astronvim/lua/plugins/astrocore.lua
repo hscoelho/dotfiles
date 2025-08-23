3caa90844d4db99dcb616cd31d63e2c1e209444b
hscoelho <henriquescharlaucoelho@gmail.com>
2025-08-23 17:53:58 -0300

    more astronvim options

--- astronvim/lua/plugins/astrocore.lua
+++ astronvim/lua/plugins/astrocore.lua
@@ -10,39 +10,42 @@ return {
   opts = {
     -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      -- large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+      -- autopairs = true, -- enable autopairs at start
+      -- cmp = true, -- enable completion at start
+      -- diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
+      -- highlighturl = true, -- highlight URLs at start
+      -- notifications = true, -- enable notifications at start
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
-      virtual_text = true,
-      underline = true,
+      -- virtual_text = true,
+      -- underline = true,
     },
     -- passed to `vim.filetype.add`
     filetypes = {
       -- see `:h vim.filetype.add` for usage
-      extension = {
-        foo = "fooscript",
-      },
-      filename = {
-        [".foorc"] = "fooscript",
-      },
-      pattern = {
-        [".*/etc/foo/.*"] = "fooscript",
-      },
+      -- extension = {
+      --   foo = "fooscript",
+      -- },
+      -- filename = {
+      --   [".foorc"] = "fooscript",
+      -- },
+      -- pattern = {
+      --   [".*/etc/foo/.*"] = "fooscript",
+      -- },
     },
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        spell = false,
+        spelllang = { "en_us", "pt_br" },
+        termguicolors = true,
+        spelloptions = "camel",
+        relativenumber = true,
+        number = true,
+        signcolumn = "yes",
+        wrap = false,
       },
       g = { -- vim.g.<key>
         -- configure global vim variables (vim.g)
@@ -71,8 +74,10 @@ return {
           desc = "Close buffer from tabline",
         },
 
-        ["<C-n>"] = { function() require("snacks").picker.buffers() end, desc = "Next buffer" },
+        ["<C-n>"] = { function() require("snacks").picker.buffers() end, desc = "Snacks buffer picker" },
 
+        ["<A-Left>"] = { "<C-O>", desc = "Go back" },
+        ["<A-Right>"] = { "<C-I>", desc = "Go forward" },
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
