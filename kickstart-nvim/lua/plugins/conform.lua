0943a8704c4bfb4fd1ab2f445c53be0cc5141ba4
hscoelho <henriquescharlaucoelho@gmail.com>
2025-08-26 15:18:33 -0300

    conform comments

--- kickstart-nvim/lua/plugins/conform.lua
+++ kickstart-nvim/lua/plugins/conform.lua
@@ -19,7 +19,7 @@ return {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, typescript = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
@@ -29,6 +29,7 @@ return {
           }
         end
       end,
+      -- TODO: Move to langs folder
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
