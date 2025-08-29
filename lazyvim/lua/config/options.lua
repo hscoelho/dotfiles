4caf3defd0b37e9774d1aae3b1fe1a3cc0ea1d27
hscoelho <henriquescharlaucoelho@gmail.com>
2025-08-29 18:19:45 -0300

    removed astronvim and lazyvim

--- lazyvim/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
-vim.opt.spell = true
-vim.opt.spelllang = { "en_us", "pt_br" }
-vim.opt.termguicolors = true
-vim.opt.spelloptions = "camel"
-vim.opt.scrolloff = 999
-vim.g.lazyvim_prettier_needs_config = true
-vim.lsp.enable("angularls")
--- lazyvim/lua/plugins/copilot.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "zbirenbaum/copilot.lua",
-  opts = {
-    -- copilot_node_command = vim.fn.expand("$HOME") .. "/.local/share/fnm/node-versions/v22.18.0/installation/bin/node",
-    type = "binary",
-  },
-}
