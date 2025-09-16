9facf7b8f484bd74f06e7021ca86688c52c91ac9
hscoelho <henriquescharlaucoelho@gmail.com>
2025-09-16 11:39:26 -0300

    changed from telescope to mini.picker

--- kickstart-nvim/lua/plugins/lspconfig.lua
+++ kickstart-nvim/lua/plugins/lspconfig.lua
@@ -40,33 +40,6 @@ return {
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, 'Code action', { 'n', 'x' })
 
-          -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-
-          -- Jump to the implementation of the word under your cursor.
-          --  Useful when your language has ways of declaring types without an actual implementation.
-          map('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-
-          -- Jump to the definition of the word under your cursor.
-          --  This is where a variable was first declared, or where a function is defined, etc.
-          --  To jump back, press <C-t>.
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
-          -- Fuzzy find all the symbols in your current document.
-          --  Symbols are things like variables, functions, types, etc.
-          map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
-
-          -- Fuzzy find all the symbols in your current workspace.
-          --  Similar to document symbols, except searches over your entire project.
-          map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
-
-          -- Jump to the type of the word under your cursor.
-          --  Useful when you're not sure what type a variable is and you want to see
-          --  the definition of its *type*, not where it was *defined*.
-          map('gt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
-
           local function client_supports_method(client, method, bufnr)
             return client:supports_method(method, bufnr)
           end
--- kickstart-nvim/lua/plugins/mini.lua
+++ kickstart-nvim/lua/plugins/mini.lua
@@ -57,6 +57,34 @@ return {
           require('mini.files').open()
         end
       end, { desc = 'File explorer' })
+
+      require('mini.pick').setup()
+      require('mini.icons').setup()
+      require('mini.extra').setup()
+
+      local map = vim.keymap.set
+      local cmd = function(cmd)
+        return '<cmd>' .. cmd .. '<cr>'
+      end
+      map('n', '<leader>ff', cmd 'Pick files', { desc = 'Mini Pick Files' })
+      map('n', '<C-n>', cmd 'Pick oldfiles', { desc = 'Mini Pick Oldfiles' })
+      map('n', '<leader>fo', cmd 'Pick oldfiles', { desc = 'Mini Pick Oldfiles' })
+      map('n', '<leader>ft', cmd 'Pick colorschemes', { desc = 'Mini Pick Theme' })
+      map('n', '<leader>fg', cmd 'Pick grep_live', { desc = 'Mini Pick Grep live' })
+      map('n', '<leader>fh', cmd 'Pick help', { desc = 'Mini Pick Help' })
+      map('n', '<leader>fk', cmd 'Pick keymaps', { desc = 'Mini Pick Keymaps' })
+      map('n', '<leader>fd', cmd 'Pick diagnostic', { desc = 'Mini Pick Diagnostics' })
+      map('n', '<leader>fr', cmd 'Pick resume', { desc = 'Mini Pick Resume' })
+      map('n', '<leader>fb', cmd 'Pick buffers', { desc = 'Mini Pick Buffers' })
+
+      -- lsp
+      map('n', 'gr', cmd "Pick lsp scope='references'", { desc = '[G]oto [R]eferences' })
+      map('n', 'gi', cmd "Pick lsp scope='implementation'", { desc = '[G]oto [I]mplementation' })
+      map('n', 'gd', cmd "Pick lsp scope='definition'", { desc = '[G]oto [D]efinition' })
+      map('n', 'gD', cmd "Pick lsp scope='declaration'", { desc = '[G]oto [D]eclaration' })
+      map('n', 'gO', cmd "Pick lsp scope='document_symbol'", { desc = 'Open Document Symbols' })
+      map('n', 'gW', cmd "Pick lsp scope='workpsace_symbol'", { desc = 'Open Workspace Symbols' })
+      map('n', 'gt', cmd "Pick lsp scope='type_definition'", { desc = '[G]oto [T]ype Definition' })
     end,
   },
 }
