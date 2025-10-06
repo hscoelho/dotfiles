5b8189b249fc32a819001c40b5f5eb37709c1b30
hscoelho <henriquescharlaucoelho@gmail.com>
2025-10-05 21:38:19 -0300

    fixed neovim completion

--- nvim/plugin/completion.lua
+++ nvim/plugin/completion.lua
@@ -4,25 +4,25 @@ end
 
 require('luasnip').setup()
 require('luasnip.loaders.from_vscode').lazy_load()
--- require('blink').setup {
---   keymap = {
---     preset = 'enter',
---   },
---   appearance = {
---     -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
---     -- Adjusts spacing to ensure icons are aligned
---     nerd_font_variant = 'normal',
---   },
---   completion = {
---     documentation = { auto_show = false, auto_show_delay_ms = 500 },
---   },
---   sources = {
---     default = { 'lsp', 'path', 'snippets', 'lazydev' },
---     providers = {
---       lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
---     },
---   },
---   snippets = { preset = 'luasnip' },
---   fuzzy = { implementation = 'prefer_rust_with_warning' },
---   signature = { enabled = true },
--- }
+require('blink.cmp').setup {
+  keymap = {
+    preset = 'enter',
+  },
+  appearance = {
+    -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+    -- Adjusts spacing to ensure icons are aligned
+    nerd_font_variant = 'normal',
+  },
+  completion = {
+    documentation = { auto_show = false, auto_show_delay_ms = 500 },
+  },
+  sources = {
+    default = { 'lsp', 'path', 'snippets', 'lazydev' },
+    providers = {
+      lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+    },
+  },
+  snippets = { preset = 'luasnip' },
+  fuzzy = { implementation = 'prefer_rust_with_warning' },
+  signature = { enabled = true },
+}
