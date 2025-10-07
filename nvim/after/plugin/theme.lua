93ac2dcd77aa5ec0cee96e9b68e1935c4d2cbd9d
hscoelho <henriquescharlaucoelho@gmail.com>
2025-10-07 13:34:07 -0300

    changed leaf comment color

--- nvim/after/plugin/theme.lua
+++ nvim/after/plugin/theme.lua
@@ -1,7 +1,13 @@
 require('kanagawa').setup {
   transparent = true,
 }
+
+local leaf_colors = require('leaf.colors').setup()
 require('leaf').setup {
   transparent = true,
+  overrides = {
+    Comment = { fg = leaf_colors.leaf_red_0 },
+  },
 }
+
 vim.cmd.colorscheme 'leaf'
