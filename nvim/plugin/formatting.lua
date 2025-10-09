ba80926615de2677f9b5e3205a0ecafbd1e56898
hscoelho <henriquescharlaucoelho@gmail.com>
2025-10-09 15:32:11 -0300

    added prettier to formatting

--- nvim/plugin/formatting.lua
+++ nvim/plugin/formatting.lua
@@ -8,7 +8,11 @@ require('conform').setup {
     -- Disable "format_on_save lsp_fallback" for languages that don't
     -- have a well standardized coding style. You can add additional
     -- languages here or re-enable it for the disabled ones.
-    local disable_filetypes = { c = true, cpp = true, typescript = true }
+    local disable_filetypes = {
+      c = true,
+      cpp = true,
+      -- typescript = true,
+    }
     if disable_filetypes[vim.bo[bufnr].filetype] then
       return nil
     else
@@ -18,13 +22,9 @@ require('conform').setup {
       }
     end
   end,
-  -- TODO: Move to langs folder
   formatters_by_ft = {
     lua = { 'stylua' },
-    -- Conform can also run multiple formatters sequentially
-    -- python = { "isort", "black" },
-    --
-    -- You can use 'stop_after_first' to run the first available formatter from the list
-    -- javascript = { "prettierd", "prettier", stop_after_first = true },
+    javascript = { 'prettierd', 'prettier', stop_after_first = true },
+    typescript = { 'prettierd', 'prettier', stop_after_first = true },
   },
 }
