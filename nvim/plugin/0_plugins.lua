788a36d9432592f934f318d1b4bf6442bb27cc9d
hscoelho <henriquescharlaucoelho@gmail.com>
2025-10-21 19:31:38 -0300

    fixed neovim

--- nvim/plugin/0_plugins.lua
+++ nvim/plugin/0_plugins.lua
@@ -24,7 +24,6 @@ require('lazy').setup {
   { 'daschw/leaf.nvim', lazy = false },
   { 'EdenEast/nightfox.nvim', lazy = false },
   { 'catppuccin/nvim', name = 'catppuccin', lazy = false },
-  { '/nvim', name = 'catppuccin', lazy = false },
   { 'neanias/everforest-nvim', lazy = false },
   -- LSP
   { 'neovim/nvim-lspconfig', lazy = false },
