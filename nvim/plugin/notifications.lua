96d99eafe5135edb2b64837b2dab9d34bc0b78ba
hscoelho <henriquescharlaucoelho@gmail.com>
2025-10-26 16:20:34 -0300

    transparent bg for fidget notifications

--- nvim/plugin/notifications.lua
+++ nvim/plugin/notifications.lua
@@ -2,4 +2,11 @@ if vim.g.vscode then
   return
 end
 
-require('fidget').setup {}
+require('fidget').setup {
+  notification = {
+    -- Options related to the notification window and buffer
+    window = {
+      winblend = 0, -- Background color opacity in the notification window
+    },
+  },
+}
