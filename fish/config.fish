dfe5261624f6e71e93eb59a850614fbc8fdb5c14
hcoelho <henriquescharlaucoelho@gmail.com>
2025-12-21 17:08:16 -0300

    added rdp aliases

--- fish/config.fish
+++ fish/config.fish
@@ -7,4 +7,8 @@ zoxide init fish | source
 mise activate fish | source
 
 alias cat="bat"
+alias rdp-win='xfreerdp /network:lan /gfx:avc444 /dynamic-resolution +clipboard'
+alias rdp-vpn='xfreerdp /network:auto /gfx:avc420 +compression +bitmap-cache +clipboard'
+
+alias rdp-docker='rdp-win /v:127.0.0.1:3389 /u:hcoelho /d:'
 
