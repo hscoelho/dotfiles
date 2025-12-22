9bb003ca41b310b0746f8699500d1dea55ad1071
hcoelho <henriquescharlaucoelho@gmail.com>
2025-12-22 09:37:46 -0300

    improved rdp aliases

--- fish/config.fish
+++ fish/config.fish
@@ -7,9 +7,9 @@ zoxide init fish | source
 mise activate fish | source
 
 alias cat="bat"
-alias rdp-win='xfreerdp /network:lan /gfx:avc444 /dynamic-resolution +clipboard'
-alias rdp-vpn='xfreerdp /network:auto /gfx:avc420 +compression +bitmap-cache +clipboard'
+alias rdp-local='xfreerdp /network:lan /gfx:avc444 /dynamic-resolution +clipboard'
+alias rdp-net='xfreerdp /network:auto /gfx:avc420 +compression /dynamic-resolution +clipboard'
 
-alias rdp-docker='rdp-win /v:127.0.0.1:3389 /u:hcoelho /d:'
-alias rdp-elipse='rdp-win /v:henrique /u:henrique /d:'
+alias rdp-vm='rdp-local /v:127.0.0.1:3389 /u:hcoelho /d:'
+alias rdp-elipse='rdp-net /v:henrique /u:henrique /d:'
 
