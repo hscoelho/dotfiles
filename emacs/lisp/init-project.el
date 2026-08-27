ac323429dee19ef2e5af499d46ac6bde286e8af1
hcoelho <henriquescharlaucoelho@gmail.com>
2026-08-26 23:38:54 -0300

    emacs: replaced project-tab-groups with auto-tab-groups

--- emacs/lisp/init-project.el
+++ emacs/lisp/init-project.el
@@ -1,17 +1,27 @@
 ;;; -*- lexical-binding: t -*-
 
 ;; alternative packages that might be worth exploring:
-;; auto-tab-groups: https://github.com/MArpogaus/auto-tab-groups
-;; tabspace: https://codeberg.org/mclear-tools/tabspaces
-;; for now, I think this simple one is exactly what I need
-(use-package project-tab-groups
-  :straight t
-  :config
-  (project-tab-groups-mode 1))
+;; tabspaces: https://codeberg.org/mclear-tools/tabspaces
 
 
-;; Switch tab-bar-format-tabs to tab-bar-format-tabs-groups
-(setq tab-bar-format '(tab-bar-format-history tab-bar-format-tabs-groups tab-bar-separator tab-bar-format-add-tab))
+(use-package auto-tab-groups
+  :straight (auto-tab-groups :type git :host github :repo "MArpogaus/auto-tab-groups")
+  :init
+  ;; A tab group for each project, made by `project-switch-project'.
+  (auto-tab-groups-project-mode)
+  ;; The modern tab style.  The icons come from nerd-icons, if you
+  ;; have it.  Plain strings also work:
+  ;; (auto-tab-groups-eyecandy-icons '(("HOME" . "⌂")))
+  (auto-tab-groups-eyecandy-mode)
+  (auto-tab-groups-mode))
+
+;; this is a simple version of the auto-tab-groups
+;; I prefer auto-tab-groups because of the eyecandy mode
+;; (use-package project-tab-groups
+;;   :straight t
+;;   :config
+;;   (project-tab-groups-mode 1))
+;; (setq tab-bar-format '(tab-bar-format-history tab-bar-format-tabs-groups tab-bar-separator))
 
 (setq project-switch-commands 'consult-project-buffer)
 (setq project-vc-extra-root-markers '(".project"))
