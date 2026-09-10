a9e389c662e861e24820991907fe1a438d8b457d
hcoelho <henriquescharlaucoelho@gmail.com>
2026-09-10 19:17:44 +0000

    emacs: add comment with coursier binary path (not using it, but might be useful for later)

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -13,6 +13,15 @@
     (setq exec-path (append exec-path (list termuxpath)))
      ;; conversion-style set to nil for evil integration
     (setq overriding-text-conversion-style nil))
+
+  ;; adding coursier bins
+  ;; (let ((coursierpath "/data/data/com.termux/files/home/.local/share/coursier/bin"))
+  ;;   (setenv "PATH" (concat (getenv "PATH") ":" coursierpath))
+  ;;   (setq exec-path (append exec-path (list coursierpath)))
+  ;;   )
+  ;; scala bins, if necessary
+  ;; export PATH=/data/data/com.termux/files/home/scala2-bins/scala-2.13.16/bin:$PATH
+  ;; export PATH=/data/data/com.termux/files/home/scala2-bins/sbt/bin:$PATH
   )
 
 (setq load-prefer-newer t)
